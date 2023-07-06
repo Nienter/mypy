e89532965245b3258a276f80eca80c45635bcf3f
blank <niupengkai@victurbo.com>
2023-07-06 09:51:29 +0800

    bing 壁纸

---
+++ personal/bingwall.py
@@ -0,0 +1,52 @@
+import json
+import urllib.request
+import requests
+import os.path
+import ctypes
+import sys
+
+print(sys.path)
+
+
+# 请求网页，跳转到最终 img 地址
+def get_img_url(raw_img_url="https://bing.biturl.top/"):
+    sys.path.append("E:\\pywork\\venv\\Lib\\site-packages\\requests")
+    r = requests.get(raw_img_url)
+    img_url = r.text  # 得到图片文件的网址
+    data = json.loads(img_url)['url']
+    print('img_url:', data)
+
+    return data
+
+
+def save_img(img_url, dirname):
+    # 保存图片到磁盘文件夹dirname中
+    try:
+        if not os.path.exists(dirname):
+            print('文件夹', dirname, '不存在，重新建立')
+            # os.mkdir(dirname)
+            os.makedirs(dirname)
+        # 获得图片文件名，包括后缀
+        basename = "bing.jpg"
+        # 拼接目录与文件名，得到图片路径
+        filepath = os.path.join(dirname, basename)
+        # 下载图片，并保存到文件夹中
+        urllib.request.urlretrieve(img_url, filepath)
+    except IOError as e:
+        print('文件操作失败', e)
+    except Exception as e:
+        print('错误 ：', e)
+    print("Save", filepath, "successfully!")
+
+    return filepath
+
+
+def set_img_as_wallpaper(filepath):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath, 0)
+
+
+if __name__ == '__main__':
+    dirname = "D:\\bingImg"  # 图片要被保存在的位置
+    img_url = get_img_url()
+    filepath = save_img(img_url, dirname)  # 图片文件的路径
+    set_img_as_wallpaper(filepath)
---
+++ personal/py_bing.bat
@@ -0,0 +1,5 @@
+@echo off
+del D:\\bingImg\*.jpg
+python bingwall.py
+
+pause
